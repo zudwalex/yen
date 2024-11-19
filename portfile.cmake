6554c97692ec17be586cf2e6102b4b4abb92dc5d
Alexander Alashkin <zudwalex@outlook.com>
2024-11-19 10:06:24 +0200

    Add vcpkg integration

---
+++ portfile.cmake
@@ -0,0 +1,13 @@
+vcpkg_from_source()
+
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/custom_dijkstra_call.hpp"
+     "${CMAKE_CURRENT_LIST_DIR}/yen_ksp.hpp"
+     DESTINATION ${CURRENT_PACKAGES_DIR}/include)
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${CMAKE_CURRENT_LIST_DIR}
+    PREFER_NINJA
+)
+
+vcpkg_install_cmake()
+vcpkg_copy_pdbs()
