49b10786d64ec590c41a6dfbb6146cf5afaa40d0
Alexander Alashkin <zudwalex@outlook.com>
2024-11-19 10:20:15 +0200

    Fix 2

--- portfile.cmake
+++ portfile.cmake
@@ -1,8 +1,9 @@
 vcpkg_from_source()
 
-file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/custom_dijkstra_call.hpp"
-     "${CMAKE_CURRENT_LIST_DIR}/yen_ksp.hpp"
-     DESTINATION ${CURRENT_PACKAGES_DIR}/include)
+file(INSTALL 
+    "${CMAKE_CURRENT_LIST_DIR}/custom_dijkstra_call.hpp"
+    "${CMAKE_CURRENT_LIST_DIR}/yen_ksp.hpp"
+    DESTINATION ${CURRENT_PACKAGES_DIR}/include)
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${CMAKE_CURRENT_LIST_DIR}
